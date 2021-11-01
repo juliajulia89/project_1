26dfe3ef29a078651a14660df591c352106fa77b
juliajulia89 <julkajanicka@gmail.com>
2021-11-01 17:19:28 +0100

    done

---
+++ js/game.js
@@ -0,0 +1,88 @@
+"use strict";
+class Game {
+  constructor() {
+    this.canvas = null;
+    this.ctx = null;
+    this.obstacles = [];
+    this.player = null;
+    this.gameIsOver = false;
+    this.score = 0;
+  }
+
+  start() {
+    // Append canvas to the DOM, create a Player and start the Canvas loop
+    // Save reference to canvas and Create ctx
+    this.canvas = document.querySelector("canvas");
+    this.ctx = canvas.getContext("2d");
+
+    // Create a new player for the current game
+    this.player = new Player(this.canvas, 3);
+
+    // Add event listener for moving the player
+    this.handleKeyDown = (event) => {
+      if (event.code === "ArrowUp") {
+        this.player.setDirection("up");
+      } else if (event.code === "ArrowDown") {
+        this.player.setDirection("down");
+      }
+    };
+
+    // Any function provided to eventListener
+    document.body.addEventListener("keydown", this.handleKeyDown);
+
+    // Start the canvas requestAnimationFrame loop
+    this.startLoop();
+  }
+
+  startLoop() {
+    const loop = () => {
+      // We create the obstacles with random y
+      if (Math.random() > 0.99) {
+        const y = Math.random() * this.canvas.height;
+        const x = this.canvas.width - 20;
+        this.obstacles.push(new Obstacle(this.ctx, x, y, 1));
+      }
+
+      // 1. UPDATE THE STATE OF PLAYER AND WE MOVE THE OBSTACLES
+      this.player.update();
+      this.obstacles.forEach((obstacle) => {
+        obstacle.move();
+      });
+
+      this.checkCollisions();
+
+      // 2. CLEAR THE CANVAS
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      // 3. UPDATE THE CANVAS
+      // Draw the player
+      this.player.draw();
+
+      // Draw the enemies
+      this.obstacles.forEach((obstacle) => {
+        obstacle.draw();
+      });
+
+      // 4. TERMINATE LOOP IF GAME IS OVER
+      if (!this.gameIsOver) {
+        window.requestAnimationFrame(loop);
+      } else {
+        buildGameOver();
+      }
+    };
+
+    // As loop function will be continuously invoked by
+    // the `window` object- `window.requestAnimationFrame(loop)`
+    // we need to `start an infinitive loop` till the game is over
+    window.requestAnimationFrame(loop);
+  }
+
+  checkCollisions() {
+    this.obstacles.forEach((obstacle) => {
+      if (this.player.didCollide(obstacle)) {
+        console.log("boom");
+        this.gameIsOver = true;
+      }
+    });
+  }
+}
