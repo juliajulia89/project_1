dbf11b19cae0245160a5c64f848083f538d60567
juliajulia89 <julkajanicka@gmail.com>
2021-11-01 16:54:04 +0100

    done

---
+++ js/index.js
@@ -0,0 +1,64 @@
+// General function that will update the HTML content dinamically
+const buildDom = (html) => {
+  const main = document.querySelector("main");
+  main.innerHTML = html;
+};
+// const themeAudio = new Audio("sound/indiana_jones.mp3");
+// First Screen => Splash Screen
+const buildSplashScreen = () => {
+  buildDom(`
+  <section class = "splash screen">
+  <h1>FIND ATLANTIS</h1>
+        <p> 
+            The success of the mission depends on you! 
+            Dr. Henry Walton aka Indiana Jones left you some tips on how to find the long lost Atlantis.
+            You have 30 seconds to get to the end of the maze. 
+            Avoid the obstacles and collect golden coins on the way to get extra time. 
+        </p>
+  <button class= "buttons" id ="start-button"> Let's go! </button>
+  </section>
+  `);
+  const startButton = document.getElementById("start-button");
+  startButton.addEventListener("click", buildGameScreen);
+};
+
+// Second Screen => Game Screen
+const buildGameScreen = () => {
+  buildDom(`
+  <section class = "game screen">
+  <div id="game-board">
+  <canvas id="canvas" width="800" height="500"></canvas>
+  </div>  
+  <button class="buttons" id="quit-button"> Quit Game</button>
+  </section>
+  `);
+
+  const quitButton = document.getElementById("quit-button");
+  quitButton.addEventListener("click", buildGameOver);
+
+  const game = new Game();
+  game.start();
+};
+
+// Third Screen => Game Over
+const buildGameOver = () => {
+  buildDom(`
+  <section class="game-over">
+ <h1>You are no Indian Jones</h1>
+        <p> 
+        </br>
+            Be faster and smarter next time. 
+        </p>
+  <button class = "buttons" id = "game"> TRY AGAIN</button>
+  <div class= "pointer"> </div>
+  </section>
+  `);
+
+  const restartButton = document.querySelector("button");
+  restartButton.addEventListener("click", buildGameScreen);
+};
+
+// When the window loads, then we will run the "buildSplashScreen" function
+// "load" waits for the html and JS
+window.addEventListener("load", buildSplashScreen);
+//
